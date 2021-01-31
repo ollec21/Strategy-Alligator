8292032f107166106561c3644a6e64c100d76459
kenorb <kenorb@users.noreply.github.com>
2021-01-31 15:58:27 +0000

    config/sets: Updates indicator params

--- config/EURUSD_M30.h
+++ config/EURUSD_M30.h
@@ -6,15 +6,15 @@
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_Alligator_Params_M30 : AlligatorParams {
   Indi_Alligator_Params_M30() : AlligatorParams(indi_alli_defaults, PERIOD_M30) {
-    applied_price = (ENUM_APPLIED_PRICE)0;
-    jaw_period = 17;
+    applied_price = (ENUM_APPLIED_PRICE)3;
+    jaw_period = 25;
     jaw_shift = 10;
-    teeth_period = 8;
-    teeth_shift = 7;
     lips_period = 7;
     lips_shift = 3;
     ma_method = (ENUM_MA_METHOD)2;
     shift = 0;
+    teeth_period = 8;
+    teeth_shift = 6;
   }
 } indi_alli_m30;
 
--- config/EURUSD_M5.h
+++ config/EURUSD_M5.h
@@ -9,12 +9,12 @@ struct Indi_Alligator_Params_M5 : AlligatorParams {
     applied_price = (ENUM_APPLIED_PRICE)0;
     jaw_period = 9;
     jaw_shift = 8;
-    teeth_period = 4;
-    teeth_shift = 5;
     lips_period = 3;
     lips_shift = 0;
     ma_method = (ENUM_MA_METHOD)2;
     shift = 0;
+    teeth_period = 4;
+    teeth_shift = 5;
   }
 } indi_alli_m5;
 
